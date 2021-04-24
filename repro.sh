974e541a4496b2d4f17bb59b3f099e67402f4ea8
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2021-04-24 20:30:40 +0200

    Matrix build

--- repro.sh
+++ repro.sh
@@ -3,5 +3,5 @@
 set -x
 set -o errexit
 
-docker build -t ansible-unarchive-issue-repro .
-docker run --tty --user user ansible-unarchive-issue-repro
+docker build --build-arg=VERSION=$1 -t ansible-unarchive-issue-repro$1 .
+docker run --tty --user user ansible-unarchive-issue-repro$1
